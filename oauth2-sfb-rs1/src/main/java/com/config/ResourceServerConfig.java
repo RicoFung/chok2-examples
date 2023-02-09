40096c499affefd76bca6297759946b9194704a3
zhuojun.feng <zhuojun.feng@moco.com>
2023-02-09 17:14:25 +0800

    202302091715 [oauth2-sfb-rs1]
    feat: 添加新资源服务[rs1]，不整合数据源

---
+++ oauth2-sfb-rs1/src/main/java/com/config/ResourceServerConfig.java
@@ -0,0 +1,64 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.config.oauth2.CusAccessDeniedHandler;
+import com.config.oauth2.CusOAuth2ExceptionEntryPoint;
+
+@EnableWebSecurity(debug = false)
+public class ResourceServerConfig
+{
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		http
+		.authorizeRequests((requests) -> requests.anyRequest().authenticated()).oauth2ResourceServer()
+		// 401 Invalid access token
+		.authenticationEntryPoint(new CusOAuth2ExceptionEntryPoint())
+		// 403 insufficient_scope
+		.accessDeniedHandler(new CusAccessDeniedHandler()).jwt();
+		return http.build();
+	}
+
+	// @Bean
+	// SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	// http
+	// .mvcMatcher("/test/**")
+	// .authorizeRequests()
+	// .mvcMatchers("/test/**").access("hasAuthority('SCOPE_test.read')")
+	// .and()
+	// .oauth2ResourceServer()
+	// .jwt();
+	// return http.build();
+	// }
+	// @Bean
+	// SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	// http
+	// .mvcMatcher("/articles/**")
+	// .authorizeRequests()
+	// .mvcMatchers("/articles/**").access("hasAuthority('SCOPE_articles.read')")
+	// .and()
+	// .oauth2ResourceServer()
+	// .jwt();
+	// return http.build();
+	// }
+	
+    @Bean
+    WebSecurityCustomizer ignore() {
+        return web -> web.ignoring().antMatchers(
+    	        "/",   
+    	        "/swagger-ui",
+    	        "/v3/api-docs/**",
+    	        "/*.html",
+    	        "/**/*.html",
+    	        "/**/*.css",
+    	        "/**/*.js",
+    	        "/**/*.png",
+    	        "/favicon.ico",
+    	        "/error");
+    }
+}
---
+++ oauth2-sfb-rs1/src/main/java/com/config/oauth2/CusOAuth2ExceptionEntryPoint.java
@@ -0,0 +1,52 @@
+package com.config.oauth2;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import com.domain.customtbdemo.dto.CustomDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import chok.common.RestConstants;
+
+public class CusOAuth2ExceptionEntryPoint implements AuthenticationEntryPoint
+{
+	private static Logger log = LoggerFactory.getLogger(CusOAuth2ExceptionEntryPoint.class);
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException
+	{
+		ObjectMapper objectMapper = new ObjectMapper();
+		// 组装数据
+		CustomDto<Object> dto = new CustomDto<Object>();
+		dto.setSuccess(false);
+		dto.setCode(RestConstants.ERROR_CODE2);
+		dto.setMsg(authException.getMessage());
+//		dto.setData(authException.getCause());
+		dto.setPath(request.getServletPath());
+		dto.setTimestamp(String.valueOf(new Date().getTime()));
+		// 返回数据
+		response.setContentType("application/json;charset=UTF-8");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		try
+		{
+			objectMapper.writeValue(response.getOutputStream(), dto);
+		}
+		catch (Exception e)
+		{
+			log.error(objectMapper.writeValueAsString(e));
+		}
+		finally
+		{
+			log.error(objectMapper.writeValueAsString(dto));
+		}
+	}
+}
