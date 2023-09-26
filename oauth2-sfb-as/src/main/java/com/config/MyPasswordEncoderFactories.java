8357066856994c36b65fb1a76bb8ff0e75604802
zhuojun.feng <zhuojun.feng@moco.com>
2023-09-26 19:31:29 +0800

    [2023/0926/19301838]
    feat: 升级chok2-parent版本号

--- oauth2-sfb-as/src/main/java/com/config/MyPasswordEncoderFactories.java
+++ oauth2-sfb-as/src/main/java/com/config/MyPasswordEncoderFactories.java
@@ -10,7 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
-import chok2.util.EncryptionUtil;
+import chok2.util.core.EncryptionUtil;
 
 public final class MyPasswordEncoderFactories
 {
